d26c69af857ce76a355644f7330e0860809c396b
prod-vs-studio <production.vsstudio@gmail.com>
2026-05-10 15:52:20 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,10 @@ import path from "path";
 import { fileURLToPath } from "url";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
 import { viteSingleFile } from "vite-plugin-singlefile";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const element = document.querySelector('.my-element');
-if (element) {
-  element.classList.add('active');
-}
 
 // https://vite.dev/config/
 export default defineConfig({
